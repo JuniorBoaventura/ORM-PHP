7904ed55c78680c172f2f99bbc3523c0e0d24bda
JuniorBoaventura <junior.boaventura1@gmail.com>
2015-12-10 16:56:47 +0100

    Getter and Setter created

--- src/model/orm/User.php
+++ src/model/orm/User.php
@@ -5,8 +5,51 @@ namespace src\model\orm;
  */
 class User extends table
 {
+  public $id;
+  public $name;
+  public $firstname;
+  public $age;
+  public $status;
 
   protected $table = "users";
 
+  public function setId($id){
+    $this->id = $id;
+  }
 
+  public function setName($name){
+    $this->name = $name;
+  }
+
+  public function setFirstname($firstname){
+    $this->firstname = $firstname;
+  }
+
+  public function setAge($age){
+    $this->age = $age;
+  }
+
+  public function setStatus($status){
+    $this->status = $status;
+  }
+
+  public function getId(){
+    return $this->id;
+  }
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function getFirstname(){
+    return $this->firstname;
+  }
+
+  public function getAge(){
+    return $this->age;
+  }
+
+  public function getStatus(){
+    return $this->status;
+  }
 }
